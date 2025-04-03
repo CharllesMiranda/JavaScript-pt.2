04829b0fe90559d87a52834f2b216f02c9e9003e
CharllesMiranda <devcharllesmiranda@gmail.com>
2025-04-03 20:54:23 -0300

    Pegando valor do meu input e colocando o que escrevo em um parágrafo.

--- script.js
+++ script.js
@@ -23,3 +23,49 @@ console.log(seletor)                        // Quiser procurar um ID colocar ("#
 
 const seletor2 = document.querySelectorAll("h1")
 console.log(seletor2)
+
+
+const element = document.querySelector(".paragraph-js")
+
+console.log(element.textContent) // Só HTML
+console.log(element.innerText)   // Leva em conta o CSS
+console.log(element.innerHTML)   // Trás TUDO -> Permite adicionar HTML (Esse é o mais comum de usar)
+
+
+const button = document.querySelector(".main-button")
+
+button.style.color = "#ffffff"
+button.style.backgroundColor = "#440000" //background-color -> Nesse exemplo tem o - entre as palavras e aqui tenho que apagar e começar a proxima palavra com letra MAIUSCULA igual no exemplo
+
+                //EVENTOS 
+
+const input2 = document.querySelector("#main-input")
+const input3 = document.querySelector(".p2")
+
+function cliqueiNoBotao () {
+    // alert("Botão Clicado com Sucesso!")
+    console.log(input2.value)
+    console.log(input3.innerText)
+    input3.innerText = (input2.value)
+}
+
+function digiteiNoInput(){
+    // console.log(input2.value)
+    console.log("Digitei no input")  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
