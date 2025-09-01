56cee8af409690a4382e24b018671d4c04903dec
CharllesMiranda <devcharllesmiranda@gmail.com>
2025-09-01 20:26:26 -0300

    Adicionando mais anotações

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //getElementsByTagName -> Trás TODOS os elementos com essa tag
 //getElementsByName -> Trás TODOS os elementos com esse NAME
 //querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
-//querySelectorAll -> Trás TODOS os elementos com esse NAME
+//querySelectorAll -> Trás TODOS os elementos com esse NOME
 
 const input = document.getElementById("main-input") 
 console.log(input)
@@ -19,17 +19,31 @@ console.log(names)
 
 const seletor = document.querySelector("p") // Quiser procurar uma classe colocar (".nomeDaClasse")
 console.log(seletor)                        // Quiser procurar um ID colocar ("#nomeDoId")
-                                            // Quiser procuar um botão com uma uma classe colocar ("button.main-button")
+                                            // Quiser procuar por exemplo um botão com uma uma classe colocar ("button.main-button"), 
+                                            // Assim se tiver um parágrafo com mesmo nome da classe, ira ignorar e buscar o primeiro botão com esse nome de classe.
 
 const seletor2 = document.querySelectorAll("h1")
 console.log(seletor2)
 
+
+const element2 = document.querySelector("#main-input")
+element2.placeholder = "Agora essa é a mensagem"        // Dessa forma consigo muda ro placeholder que antes era "Digite aqui"
+
+
+/* Alterando e Acessando textos*/
+
+/*
+    textContent -> Pega todo o conteúdo
+    innerText -> Pega APENAS o texto
+    innerHTML -> Permite adicionar HTML e texto
+*/
 
 const element = document.querySelector(".paragraph-js")
 
 console.log(element.textContent) // Só HTML
 console.log(element.innerText)   // Leva em conta o CSS
 console.log(element.innerHTML)   // Trás TUDO -> Permite adicionar HTML (Esse é o mais comum de usar)
+element.innerHTML = "Texto Novo <b>Olá</b>" //Consegue adicionar html na alteração, nesse exemplo a tag b
 
 
 const button = document.querySelector(".main-button")
@@ -53,19 +67,4 @@ function digiteiNoInput(){
     // console.log(input2.value)
     console.log("Digitei no input")  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ script2.js
@@ -0,0 +1,27 @@
+            // addEventListener
+
+const input = document.querySelector("#main-input")
+const select = document.querySelector("select")
+const button = document.querySelector(".main-button")
+
+//change -> É quando eu troco de valor
+//function(Esta vazia pq nao precisei colocar nome por exempo no meu html)
+//Essa é a estrutura do addEventListener
+
+select.addEventListener("change", function(){    // addEventListener
+    console.log("Troquei de valor")
+})
+
+            //Outra forma de usar o addEventListener para pegar muitas opções do evento
+
+function troqueiValor (event){
+    console.log(event) 
+}
+
+select.addEventListener("change", troqueiValor)
+
+// Posso fazer assim também o que esta na linha 17 até 21
+
+select.addEventListener("change", function(event){
+    console.log(event)
+})
